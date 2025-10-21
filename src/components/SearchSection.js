d2c386481ed24934070fb9ae40332d8ed6023bfa
Plamen <plamenkosharov777@gmail.com>
2025-10-21 22:11:37 +0300

    Added search bar and search button functionality

--- src/components/SearchSection.js
+++ src/components/SearchSection.js
@@ -1,8 +1,38 @@
 import styles from "../styles/SearchSection.module.css"
 import {useState} from "react";
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+const URL = "https://api.calorieninjas.com/v1/nutrition?query="
+
+
 export default function SearchSection(){
   const [search, setSearch] = useState("")
+
+
+  async function handleSearch(){
+    if (search.trim() === ""){
+      alert("Please enter food name.");
+      return;
+    }
+
+    try{
+      const response = await fetch(`${URL}${(search)}`, {
+        method: "GET",
+        headers: {
+          "X-Api-Key": API_KEY
+        },
+      });
+
+      if (!response.ok){
+        throw new Error(response.statusText);
+      }
+      const data = await response.json()
+      console.log(data)
+    }
+    catch (error) {
+      alert(error.message)
+    }
+  }
 
   return(
     <div className={styles.searchSection}>
@@ -13,7 +43,7 @@ export default function SearchSection(){
         onChange={(e) => setSearch(e.target.value)}
         value={search}
       />
-      <button className={styles.searchButton}>Search</button>
+      <button onClick={handleSearch} className={styles.searchButton}>Search</button>
     </div>
   )
 }
